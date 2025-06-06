04423d733f76038937d7c8853364c06a18af25c1
Felipe Silva <felipe.silva@webtech87.pt>
2025-06-06 12:32:32 +0100

    [UPDATE] Checkout ScrollFadeWrapper.tsx from main, add ID parameter to Scroll and to App and update videos in videoslider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import PrivacyPolicy from './pages/privacy-policy';
 import CookiesPolicy from './pages/cookies-policy';
 import TermsConditions from './pages/terms-conditions/TermsConditions';
 import VideoTestimonials from './components/videoslider';
+import ScrollFadeWrapper from './components/scroll-fade-wrapper';
 import './App.css';
 
 function App() {
@@ -25,13 +26,36 @@ function App() {
           <Route path="/" element={
             <>
               <Navbar />
-              <Hero />
-              <WhyChoose />
-              <Treatments />
-              <VideoTestimonials /> 
-              <Testimonials />
-              <Contact />
-              <Footer />
+              {/* Hero - animate once since it's the first section */}
+              <ScrollFadeWrapper animateOnce={true}>
+                <Hero />
+              </ScrollFadeWrapper>
+              
+              {/* Main sections - bidirectional animations */}
+              <ScrollFadeWrapper id='why-choose' delay={0} direction="up">
+                <WhyChoose />
+              </ScrollFadeWrapper>
+              
+              <ScrollFadeWrapper id='treatments' delay={0} direction="up">
+                <Treatments />
+              </ScrollFadeWrapper>
+              
+              <ScrollFadeWrapper delay={0} direction="up">
+                <VideoTestimonials />
+              </ScrollFadeWrapper>
+              
+              <ScrollFadeWrapper delay={0} direction="up">
+                <Testimonials />
+              </ScrollFadeWrapper>
+              
+              <ScrollFadeWrapper id='contact' delay={0} direction="up">
+                <Contact />
+              </ScrollFadeWrapper>
+              
+              {/* Footer - animate once since it's the last section */}
+              <ScrollFadeWrapper delay={0} direction="up" animateOnce={true}>
+                <Footer />
+              </ScrollFadeWrapper>
               <WhatsAppFloat />
               <PopupForm />
               <CookiesBanner />
@@ -42,8 +66,13 @@ function App() {
           <Route path="/politica-privacidade" element={
             <>
               <Navbar />
-              <PrivacyPolicy />
-              <Footer />
+              <ScrollFadeWrapper>
+                <PrivacyPolicy />
+              </ScrollFadeWrapper>
+              <ScrollFadeWrapper delay={200} animateOnce={true}>
+                <Footer />
+              </ScrollFadeWrapper>
+
               <WhatsAppFloat />
               <CookiesBanner />
             </>
@@ -55,6 +84,12 @@ function App() {
               <Navbar />
               <CookiesPolicy />
               <Footer />
+              <ScrollFadeWrapper>
+                <CookiesPolicy />
+              </ScrollFadeWrapper>
+              <ScrollFadeWrapper delay={200} animateOnce={true}>
+                <Footer />
+              </ScrollFadeWrapper>
               <WhatsAppFloat />
               <CookiesBanner />
             </>
@@ -64,8 +99,12 @@ function App() {
           <Route path="/termos-condicoes" element={
             <>
               <Navbar />
-              <TermsConditions />
-              <Footer />
+              <ScrollFadeWrapper>
+                <TermsConditions />
+              </ScrollFadeWrapper>
+              <ScrollFadeWrapper delay={200} animateOnce={true}>
+                <Footer />
+              </ScrollFadeWrapper>
               <WhatsAppFloat />
               <CookiesBanner />
             </>
